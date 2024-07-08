689ef3eb29923668a497966a4d4ced766390086a
raoulvdberge <raoulvdberge@gmail.com>
2024-07-08 20:30:24 +0200

    fix: missing publication

--- refinedarchitect-plugin/build.gradle.kts
+++ refinedarchitect-plugin/build.gradle.kts
@@ -33,3 +33,24 @@ dependencies {
     implementation("net.darkhax.curseforgegradle:CurseForgeGradle:1.1.18")
     implementation("com.modrinth.minotaur:Minotaur:2.8.7")
 }
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = project.uri("https://maven.pkg.github.com/" + System.getenv("GITHUB_REPOSITORY"))
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+        maven {
+            name = "CreeperHost"
+            url = project.uri("https://maven.creeperhost.net/release")
+            credentials {
+                username = System.getenv("CREEPERHOST_MAVEN_USERNAME")
+                password = System.getenv("CREEPERHOST_MAVEN_TOKEN")
+            }
+        }
+    }
+}
