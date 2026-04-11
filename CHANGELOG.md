a5635a09025706fe765d08b147691d13d5d1e662
raoulvdberge <raoulvdberge@gmail.com>
2026-04-11 08:57:46 +0200

    feat: port to minecraft 26.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
 
+### Added
+
+-   Ported to Minecraft 26.1.2.
+
 ## [1.5.0] - 2026-03-28
 
 ### Added
--- refinedarchitect-plugin/src/main/kotlin/versions.kt
+++ refinedarchitect-plugin/src/main/kotlin/versions.kt
@@ -1,9 +1,9 @@
-const val mcVersion = "26.1"
+const val mcVersion = "26.1.2"
 // See: https://projects.neoforged.net/neoforged/neoforge
-const val neoForgeVersion = "26.1.0.7-beta"
+const val neoForgeVersion = "26.1.2.4-beta"
 // See: https://fabricmc.net/develop/
-const val fabricLoaderVersion = "0.18.5"
-const val fabricApiVersion = "0.144.3+26.1"
+const val fabricLoaderVersion = "0.18.6"
+const val fabricApiVersion = "0.145.4+26.1.2"
 // See: https://projects.neoforged.net/neoforged/neoform
-const val nfVersion = "26.1-1"
+const val nfVersion = "26.1.2-1"
 const val javaVersion = 25
